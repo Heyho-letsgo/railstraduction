9a6fc45ba80a3d53058020faddb6d82d0d46f380
andre <abaudin@gmail.com>
2015-06-23 09:40:50 +0200

    Rajout du placeholder dans nav

--- tipuesearch/tipuesearch_set.js
+++ tipuesearch/tipuesearch_set.js
@@ -1,45 +1,50 @@
+/*
+ Tipue Search 5.0
+ Copyright (c) 2015 Tipue
+ Tipue Search is released under the MIT License
+ http://www.tipue.com/search
+ */
+
 
 /*
-Tipue Search 5.0
-Copyright (c) 2015 Tipue
-Tipue Search is released under the MIT License
-http://www.tipue.com/search
-*/
-
-
-/*
-Stop words
-Stop words list from http://www.ranks.nl/stopwords
-*/
+ Stop words
+ Stop words list from http://www.ranks.nl/stopwords
+ */
 
 var tipuesearch_stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
 
 
 // Word replace
 
-var tipuesearch_replace = {'words': [
-     {'word': 'tipua', 'replace_with': 'tipue'},
-     {'word': 'javscript', 'replace_with': 'javascript'},
-     {'word': 'jqeury', 'replace_with': 'jquery'}
-]};
+var tipuesearch_replace = {
+    'words': [
+        {'word': 'tipua', 'replace_with': 'tipue'},
+        {'word': 'javscript', 'replace_with': 'javascript'},
+        {'word': 'jqeury', 'replace_with': 'jquery'}
+    ]
+};
 
 
 // Weighting
 
-var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 200},
-     {'url': 'http://www.tipue.com/search', 'score': 100},
-     {'url': 'http://www.tipue.com/about', 'score': 100}
-]};
+var tipuesearch_weight = {
+    'weight': [
+        {'url': 'http://www.tipue.com', 'score': 200},
+        {'url': 'http://www.tipue.com/search', 'score': 100},
+        {'url': 'http://www.tipue.com/about', 'score': 100}
+    ]
+};
 
 
 // Stemming
 
-var tipuesearch_stem = {'words': [
-     {'word': 'e-mail', 'stem': 'email'},
-     {'word': 'javascript', 'stem': 'jquery'},
-     {'word': 'javascript', 'stem': 'js'}
-]};
+var tipuesearch_stem = {
+    'words': [
+        {'word': 'e-mail', 'stem': 'email'},
+        {'word': 'javascript', 'stem': 'jquery'},
+        {'word': 'javascript', 'stem': 'js'}
+    ]
+};
 
 
 // Internal strings
@@ -48,7 +53,7 @@ var tipuesearch_string_1 = 'No title';
 var tipuesearch_string_2 = 'Showing results for';
 var tipuesearch_string_3 = 'Search instead for';
 var tipuesearch_string_4 = '1 result';
-var tipuesearch_string_5 = 'results';
+var tipuesearch_string_5 = 'résultats';
 var tipuesearch_string_6 = 'Prev';
 var tipuesearch_string_7 = 'Next';
 var tipuesearch_string_8 = 'Nothing found';
@@ -63,7 +68,7 @@ var tipuesearch_pages = ["https://heyho-letsgo.github.io/railstraduction/index.h
     "https://heyho-letsgo.github.io/railstraduction/onepage.html",
     "https://heyho-letsgo.github.io/railstraduction/",
     "https://heyho-letsgo.github.io/railstraduction/0102_Creer_l_application_rails.html",
-"https://heyho-letsgo.github.io/railstraduction/0103_Views_et_Controllers.html",
+    "https://heyho-letsgo.github.io/railstraduction/0103_Views_et_Controllers.html",
     "https://heyho-letsgo.github.io/railstraduction/0104_Rails_Les_modeles.html",
     "https://heyho-letsgo.github.io/railstraduction/0105_Rails_Connexion_du_MVC.html",
     "https://heyho-letsgo.github.io/railstraduction/0106_Rails_Migration.html"
